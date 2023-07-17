b10456d035f0e3bc33ae29740eea305de2c8d30b
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2023-07-18 02:32:50 +0500

    serving react app with express

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
+const path = require("path");
 const express = require("express");
+const userRouter = require("./Node/routes/user/user.routes");
 require("dotenv").config();
 const app = express();
-app.use(express.json());
-const userRouter = require("./Node/routes/user/user.routes");
-app.use("/users", userRouter);
 
+app.use(express.static(path.join(__dirname, "..", "server", "public")));
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.get("*/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "server", "public", "index.html"));
+});
 module.exports = app;
