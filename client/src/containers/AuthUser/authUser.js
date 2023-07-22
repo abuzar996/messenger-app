efac28e72e8710d74219fd9a0a05478c8cf95c73
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2023-07-23 00:53:24 +0500

    changing app logo

--- client/src/containers/AuthUser/authUser.js
+++ client/src/containers/AuthUser/authUser.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+
 const AuthUser = ({ children }) => {
   const navigate = useNavigate();
   const { isAuthenticated } = useSelector((state) => state.authReducer);
