398548a914f94c85f914389db98846d9cc14b358
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2023-08-05 04:05:18 +0500

    removing unwanted files

--- client/src/components/Notifications/worker.js
+++
@@ -1,7 +0,0 @@
-console.log("This is worker file");
-onmessage = (event) => {
-  if (event.data === "start") {
-    // Call your time-based function here
-    //    scheduleTimeBasedFunction();
-  }
-};
